cffa306aee425322785ed4f38895e65395c182f1
Mazhara87 <olgamazhara87@gmail.com>
2023-07-03 09:19:16 +0200

    ex7

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="user.php" metod="get">
+    <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom" required><br>
+    <label for="prenom">Prenom :</label>
+        <input type="text" name="prenom" id="prenom" required><br>
+        <input type="submit" value="Envoyer">
+</form>
+</body>
+</html>
